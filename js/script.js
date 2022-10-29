967d6223ba18ca23116088ef23bb3a22a27e917e
tyrone17 <tyronemaasdorp@gmail.com>
2022-10-29 23:18:10 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -1,36 +1,13 @@
 var serial = Date.now();
-let form = document.forms['contact-form'];
-let formradios = document.getElementsByClassName('form-check-input');
 
  function setSerial() {
-     const serialNo = "TSB_" + serial;
-     document.getElementById('check').value = serialNo;
+    var serialNo = "TSB_" + serial;
+    document.getElementById('check').value = serialNo;
  };
 
  function message () {
-    if (confirm("Confirm that you understand that you are asking us for help and accept our T&Cs!") == true) {
-        
-    } else {
-        response = "You canceled, next time perhaps!"
-     }
-     window.location("/index1.html");
- }
+    alert("Your request" + serialNo + " was logged successfully!")
+ };
 
-// function radioValidation() {
-//     //.form-check-input:checked
-    
-    
-//     // formradios.addEventListener('onchange', e => {
-//     //     e.preventDefault()
-//     //   })
 
-// }
 
-{% comment %} var test = document.getElementsByClassName('form-check-input');
-let tar = test[0].checked
-
-if (tar = true) {
-    console.log(tar)
-} else {
-    console
-} {% endcomment %}
