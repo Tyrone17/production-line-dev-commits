ca5aede4ae644262ea44c463a1099a301dda3198
tyrone17 <tyronemaasdorp@gmail.com>
2022-10-29 23:20:59 +0200

    edited js file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 var serial = Date.now();
-<<<<<<< HEAD
-=======
-let form = document.forms['contact-form'];
 //let formradios = document.getElementsByClassName('form-check-input');
->>>>>>> 042523537661490ccf9f4be9834026bc4299c310
 
  function setSerial() {
     var serialNo = "TSB_" + serial;
@@ -11,13 +7,5 @@ let form = document.forms['contact-form'];
  };
 
  function message () {
-<<<<<<< HEAD
     alert("Your request" + serialNo + " was logged successfully!")
  };
-
-
-
-=======
-    alert("Form is locked and loaded for one our Support agents - thank you!");
- }
->>>>>>> 042523537661490ccf9f4be9834026bc4299c310
